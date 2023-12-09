36c2de5d7bc950ee5fa12a7209498ecd340928a4
TwoShot22 <mansoo0621@naver.com>
2023-12-10 01:06:54 +0900

    feat: craco alias를 활용해 경로 alias를 설정한다

---
+++ craco.config.cjs
@@ -0,0 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+
+module.exports = {
+	plugins: [
+		{
+			plugin: {
+				overrideWebpackConfig: ({ webpackConfig }) => {
+					webpackConfig.resolve.plugins.push(new TsconfigPathsPlugin({}))
+					return webpackConfig
+				},
+			},
+		},
+	],
+}
