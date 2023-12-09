82bd23ac5f9a82760d6f78ada6a59afca279fb7d
TwoShot22 <mansoo0621@naver.com>
2023-12-10 01:50:36 +0900

    refactor: component import 경로를 alias 형식으로 대체한다

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import MainLayout from '../components/layout/MainLayout'
-import AdminLayout from '../components/layout/AdminLayout'
-import User from '../pages/user/User'
-import Home from '../pages/Home'
-import CouponCreate from '../pages/admin/Create'
-import UsedDashboard from '../pages/admin/UsedDashboard'
-import CreateDashboard from '../pages/admin/CreateDashboard'
-import NotFound from '../pages/NotFound'
+import MainLayout from '@/components/layout/MainLayout'
+import AdminLayout from '@/components/layout/AdminLayout'
+import User from '@/pages/user/User'
+import Home from '@/pages/Home'
+import CouponCreate from '@/pages/admin/Create'
+import UsedDashboard from '@/pages/admin/UsedDashboard'
+import CreateDashboard from '@/pages/admin/CreateDashboard'
+import NotFound from '@/pages/NotFound'
 
 const Router = () => {
 	return (
