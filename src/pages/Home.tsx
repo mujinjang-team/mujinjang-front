f4356dfdcca027458cb709114dc2202f9f78483d
TwoShot22 <mansoo0621@naver.com>
2023-12-11 01:30:54 +0900

    feat: Index 페이지를 구현한다

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import Button from '@/components/common/Button'
+import { Link } from 'react-router-dom'
+import { HomeContainer } from '@/components/user/home/Home.styles'
 
 const Home = () => {
-	return <>Home</>
+	return (
+		<HomeContainer>
+			<Link to="/user/1">
+				<Button variant="blue">유저 - 쿠폰 사용</Button>
+			</Link>
+			<Link to="/admin/create">
+				<Button variant="blue">어드민 - 쿠폰 발급</Button>
+			</Link>
+			<Link to="/admin/coupon/created">
+				<Button variant="blue">어드민 - 쿠폰 발급 내역 확인</Button>
+			</Link>
+			<Link to="/admin/coupon/used">
+				<Button variant="blue">어드민 - 쿠폰 사용 내역 확인</Button>
+			</Link>
+		</HomeContainer>
+	)
 }
 
 export default Home
