6e26adae1749ae6ec9992f85f9a258b7ae797368
TwoShot22 <mansoo0621@naver.com>
2023-12-11 02:12:23 +0900

    feat: 쿠폰 발급 수량 input의 value를 버튼 클릭 시 감지하도록 연동한다

--- src/pages/admin/Create.tsx
+++ src/pages/admin/Create.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import {
 	CouponCreateButtonContainer,
 	CouponCreateContainer,
@@ -9,17 +9,20 @@ import {
 import Button from '@/components/common/Button'
 
 const CouponCreate = () => {
+	const inputRef = useRef<HTMLInputElement>()
+
 	return (
 		<CouponCreateContainer>
 			<CreateAmountInputContainer>
 				<CreateAmountGuideText>발급할 쿠폰의 수량 입력</CreateAmountGuideText>
-				<CreateAmountInput />
+				<CreateAmountInput inputRef={inputRef} defaultValue={0} />
 			</CreateAmountInputContainer>
 			<CouponCreateButtonContainer>
 				<Button
 					variant="black"
 					onClick={() => {
 						console.log('Clicked')
+						console.log(inputRef.current?.value)
 					}}
 				>
 					쿠폰 사용하기
