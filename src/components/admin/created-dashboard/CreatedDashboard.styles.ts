084f10b1f6fe168d9b318ec4cab044edde810e8a
TwoShot22 <mansoo0621@naver.com>
2023-12-11 02:23:57 +0900

    ui: 쿠폰 발급 현황 페이지 레이아웃과 헤더를 구현한다

---
+++ src/components/admin/created-dashboard/CreatedDashboard.styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const CreatedDashboardContainer = styled.div`
+	display: flex;
+	width: 100%;
+	height: 100%;
+	flex-direction: column;
+	gap: 12px;
+	padding: 12px;
+`
+
+export const CreatedDashboardHeaderContainer = styled.div`
+	display: flex;
+`
+
+export const CreatedDashboardHeader = styled.span`
+	font-size: 28px;
+	font-weight: 700;
+`
+
+export const CreatedDashboardContentContainer = styled.div`
+	display: flex;
+`
--- src/pages/admin/CreatedDashboard.tsx
+++ src/pages/admin/CreatedDashboard.tsx
@@ -1,7 +1,20 @@
+import {
+	CreatedDashboardContainer,
+	CreatedDashboardHeaderContainer,
+	CreatedDashboardContentContainer,
+	CreatedDashboardHeader,
+} from '@/components/admin/created-dashboard/CreatedDashboard.styles'
 import React from 'react'
 
 const CreatedDashboard = () => {
-	return <>Admin CreatedDashboard</>
+	return (
+		<CreatedDashboardContainer>
+			<CreatedDashboardHeaderContainer>
+				<CreatedDashboardHeader>쿠폰 생성 현황</CreatedDashboardHeader>
+			</CreatedDashboardHeaderContainer>
+			<CreatedDashboardContentContainer></CreatedDashboardContentContainer>
+		</CreatedDashboardContainer>
+	)
 }
 
 export default CreatedDashboard
