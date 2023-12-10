a69191099db1fd4d53548b9c331347b06665a4f5
TwoShot22 <mansoo0621@naver.com>
2023-12-11 00:49:13 +0900

    feat: Mujinjang Logo 컴포넌트를 구현한다

---
+++ src/components/common/MujinjangLogo/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import MujinjangLogoImage from '@/assets/images/mujinjang-logo.svg'
+
+const MujinjangLogo = () => {
+	return <img src={MujinjangLogoImage} alt="Mujinjang Logo" />
+}
+
+export default MujinjangLogo
