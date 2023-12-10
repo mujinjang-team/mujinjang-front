8ce3043311fdb2eec155810be2c57b27a79f0602
TwoShot22 <mansoo0621@naver.com>
2023-12-11 00:48:20 +0900

    style: Normalize css를 업데이트한다

--- src/styles/normalize.ts
+++ src/styles/normalize.ts
@@ -1,14 +1,19 @@
 import { createGlobalStyle } from 'styled-components'
 
 const Normalize = createGlobalStyle`
-	body {
+	html,
+	body,
+	#root,
+	#root > .App {
+		width: 100%;
+		height: 100%;
 		font-size: 14px;
 		font-weight: 400;
 		color: #000000;
 	}
 	
 	body * {
-        font-family: Pretendard, Pretendard-Regular, -apple-system, BlinkMacSystemFont, "Segoe UI", "맑은 고딕", "Malgun Gothic",
+        font-family: Pretendard, -apple-system, BlinkMacSystemFont, "Segoe UI", "맑은 고딕", "Malgun Gothic",
         Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
 		box-sizing: border-box;
 		-webkit-tap-highlight-color: transparent;
