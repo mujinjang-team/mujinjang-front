8a1433b368782b058bfa13fa8f996cf41af7b275
TwoShot22 <mansoo0621@naver.com>
2023-12-11 01:49:33 +0900

    refactor: Home Component 디렉토리를 수정한다

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Button from '@/components/common/Button'
 import { Link } from 'react-router-dom'
-import { HomeContainer } from '@/components/user/home/Home.styles'
+import { HomeContainer } from '@/components/home/Home.styles'
 
 const Home = () => {
 	return (
