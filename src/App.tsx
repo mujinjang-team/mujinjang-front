ac1c5a5212cf3c5d277c3b6194d1e85acd5a64ce
TwoShot22 <mansoo0621@naver.com>
2023-12-13 20:35:58 +0900

    feat: React-Query를 활용할 수 있도록 전역 설정을 추가한다

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,26 @@ import React from 'react'
 import Router from './routes/Router'
 import Reset from '@/styles/reset'
 import Normalize from '@/styles/normalize'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 function App() {
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			queries: {
+				refetchOnWindowFocus: false,
+				retry: 2,
+			},
+		},
+	})
+
 	return (
-		<>
+		<QueryClientProvider client={queryClient}>
 			<Reset />
 			<Normalize />
 			<div className="App">
 				<Router />
 			</div>
-		</>
+		</QueryClientProvider>
 	)
 }
 
