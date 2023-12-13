163d1d6730c5f3152a1be84394083cdb256dfa96
TwoShot22 <mansoo0621@naver.com>
2023-12-13 20:51:51 +0900

    feat: 404 페이지 내부 UI를 구현한다

---
+++ src/components/not-found/NotFound.styles.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components'
+
+export const NotFoundWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	gap: 2rem;
+	padding-top: 8rem;
+`
+
+export const NotFoundTitleWrapper = styled.div`
+	display: flex;
+	font-size: 4rem;
+`
+
+export const NotFoundSubTitleWrapper = styled.div`
+    display; flex;
+    font-size: 1.5rem;
+`
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,17 @@
+import { NotFoundSubTitleWrapper, NotFoundTitleWrapper, NotFoundWrapper } from '@/components/not-found/NotFound.styles'
 import React from 'react'
 
 const NotFound = () => {
-	return <>404</>
+	return (
+		<NotFoundWrapper>
+			<NotFoundTitleWrapper>
+				<span>4</span>
+				<span>🤔</span>
+				<span>4</span>
+			</NotFoundTitleWrapper>
+			<NotFoundSubTitleWrapper>요청하신 페이지를 찾을 수 없습니다</NotFoundSubTitleWrapper>
+		</NotFoundWrapper>
+	)
 }
 
 export default NotFound
