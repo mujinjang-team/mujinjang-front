f455a4404babf257bd2a7e5feea657e04b1631fc
TwoShot22 <mansoo0621@naver.com>
2023-12-17 15:06:58 +0900

    feat: CouponList 컴포넌트의 Empty View를 구현한다

---
+++ src/components/coupon/CouponList.styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+export const CouponListWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	height: 100%;
+`
+
+export const CouponListEmptyViewWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	height: 100%;
+	justify-content: center;
+	align-items: center;
+	gap: 32px;
+`
+
+export const CouponListEmptyViewTitle = styled.div`
+	font-size: 2rem;
+	font-weight: bold;
+`
+
+export const CouponListEmptyViewSubTitle = styled.p`
+	font-size: 1rem;
+	font-weight: 500;
+`
--- src/pages/coupon/CouponList.tsx
+++ src/pages/coupon/CouponList.tsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {
+	CouponListWrapper,
+	CouponListEmptyViewWrapper,
+	CouponListEmptyViewTitle,
+	CouponListEmptyViewSubTitle,
+} from '@/components/coupon/CouponList.styles'
 
 const CouponList = () => {
-	return <></>
+	const [couponList] = useState([])
+	if (couponList.length === 0) {
+		return (
+			<CouponListEmptyViewWrapper>
+				<CouponListEmptyViewTitle>발급받을 수 있는 쿠폰이 없어요 🥲</CouponListEmptyViewTitle>
+				<CouponListEmptyViewSubTitle>새로운 쿠폰이 등장하길 기다려 주세요!</CouponListEmptyViewSubTitle>
+			</CouponListEmptyViewWrapper>
+		)
+	}
+
+	return <CouponListWrapper></CouponListWrapper>
 }
 
 export default CouponList
