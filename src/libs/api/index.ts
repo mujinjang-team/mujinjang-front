edbca338ff40df501ea6e115a897b1829089492b
TwoShot22 <mansoo0621@naver.com>
2023-12-18 00:31:32 +0900

    fix: api endpoint 변경에 따라 axois baseUrl을 수정한다

--- src/libs/api/index.ts
+++ src/libs/api/index.ts
@@ -2,7 +2,7 @@ import { handleError, responseData } from './common'
 import { getBaseUrl } from './common'
 import { Api } from './Api'
 
-const api = new Api({ baseURL: getBaseUrl() })
+const api = new Api({ baseURL: getBaseUrl() + '/api' })
 
 api.instance.interceptors.request.use(undefined, handleError)
 api.instance.interceptors.response.use(responseData, handleError)
