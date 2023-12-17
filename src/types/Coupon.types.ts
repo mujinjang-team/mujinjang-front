87dd31c53d80639c4c8b34272ab05196360a075f
TwoShot22 <mansoo0621@naver.com>
2023-12-17 16:05:51 +0900

    feat: Coupon 공통 Type을 정의한다

--- src/types/Coupon.types.ts
+++ src/types/Coupon.types.ts
@@ -1,3 +1,13 @@
+export type CouponDataType = {
+	couponId: number
+	name: string
+	code: string
+	type: string
+	discount: number
+	amount: number
+	createdAt: string
+}
+
 export type CouponStatusType =
 	| 'COUPON_CREATE_AVAILABLE'
 	| 'COUPON_USE_AVAILABLE'
