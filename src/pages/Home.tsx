d994994d51fd2eae7e03751998531baf53059283
TwoShot22 <mansoo0621@naver.com>
2023-12-17 18:06:23 +0900

    feat: Index 페이지에서 user seq를 올바르게 입력하면 localStorage에 저장하고 리다이렉트 되는 로직을 구현한다

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react'
 import Button from '@/components/common/Button'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import {
 	AdminContainer,
 	HomeContainer,
@@ -10,10 +10,24 @@ import {
 	UserSeqInput,
 } from '@/components/home/Home.styles'
 import { getRandom } from '@/utils'
+import localStorage from '@/libs/localStorage'
 
 const Home = () => {
 	const defaultUserSeq = getRandom(1, 10 * 1000 * 1000)
 	const inputRef = useRef<HTMLInputElement>()
+	const navigate = useNavigate()
+
+	const redirectWithUserSeq = () => {
+		const value = Number(inputRef.current?.value)
+		console.log(value)
+		if (!value || value < 1 || value > 10 * 1000 * 1000) {
+			alert('올바른 형식의 User ID를 입력해주세요')
+			return
+		}
+
+		localStorage.setItem('user', value)
+		navigate('/coupon/list')
+	}
 
 	return (
 		<HomeContainer>
@@ -21,9 +35,9 @@ const Home = () => {
 				<SectionTitle>User</SectionTitle>
 				<UserContainer>
 					<UserSeqInput inputRef={inputRef} defaultValue={defaultUserSeq} />
-					<Link to="/coupon/list">
-						<Button variant="blue">선택된 User로 로그인</Button>
-					</Link>
+					<Button variant="blue" onClick={redirectWithUserSeq}>
+						선택된 User로 로그인
+					</Button>
 				</UserContainer>
 			</SectionWrapper>
 
