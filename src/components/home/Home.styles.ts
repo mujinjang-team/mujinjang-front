fc5efd60dd5571a739ff2169eff51d3f7c504759
TwoShot22 <mansoo0621@naver.com>
2023-12-17 17:59:34 +0900

    feat: Index 페이지에서 user seq를 입력하고 해당 user로 진입할 수 있도록 구현한다

--- src/components/home/Home.styles.ts
+++ src/components/home/Home.styles.ts
@@ -1,3 +1,4 @@
+import { TextField } from '@mui/material'
 import styled from 'styled-components'
 
 export const HomeContainer = styled.div`
@@ -5,11 +6,42 @@ export const HomeContainer = styled.div`
 	width: 100%;
 	height: 100%;
 	flex-direction: column;
-	justify-content: center;
-	padding: 0 12px;
+	justify-content: space-between;
+	padding: 20px 12px;
 	gap: 20px;
 
 	* {
 		width: 100%;
 	}
+`
+
+export const SectionWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 20px;
+`
+
+export const SectionTitle = styled.div`
+	font-size: 2rem;
+	font-weight: bold;
+`
+
+export const UserContainer = styled.div`
+	display: flex;
+	gap: 20px;
+	align-items: center;
+`
+
+export const UserSeqInput = styled(TextField).attrs({
+	type: 'number',
+	placeholder: 'User Id를 입력해 주세요',
+})`
+	* {
+		font-size: 20px !important;
+	}
+`
+
+export const AdminContainer = styled.div`
+	display: flex;
+	gap: 20px;
 `
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,43 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Button from '@/components/common/Button'
 import { Link } from 'react-router-dom'
-import { HomeContainer } from '@/components/home/Home.styles'
+import {
+	AdminContainer,
+	HomeContainer,
+	SectionTitle,
+	SectionWrapper,
+	UserContainer,
+	UserSeqInput,
+} from '@/components/home/Home.styles'
+import { getRandom } from '@/utils'
 
 const Home = () => {
+	const defaultUserSeq = getRandom(1, 10 * 1000 * 1000)
+	const inputRef = useRef<HTMLInputElement>()
+
 	return (
 		<HomeContainer>
-			<Link to="/coupon/list">
-				<Button variant="blue">유저 - 쿠폰 사용</Button>
-			</Link>
-			<Link to="/admin/coupon/list">
-				<Button variant="blue">어드민 - 쿠폰 내역</Button>
-			</Link>
-			<Link to="/admin/coupon/create">
-				<Button variant="blue">어드민 - 쿠폰 생성</Button>
-			</Link>
+			<SectionWrapper>
+				<SectionTitle>User</SectionTitle>
+				<UserContainer>
+					<UserSeqInput inputRef={inputRef} defaultValue={defaultUserSeq} />
+					<Link to="/coupon/list">
+						<Button variant="blue">선택된 User로 로그인</Button>
+					</Link>
+				</UserContainer>
+			</SectionWrapper>
+
+			<SectionWrapper>
+				<SectionTitle>Admin</SectionTitle>
+				<AdminContainer>
+					<Link to="/admin/coupon/list">
+						<Button variant="black">어드민 - 쿠폰 내역</Button>
+					</Link>
+					<Link to="/admin/coupon/create">
+						<Button variant="black">어드민 - 쿠폰 생성</Button>
+					</Link>
+				</AdminContainer>
+			</SectionWrapper>
 		</HomeContainer>
 	)
 }
