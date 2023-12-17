bf016dd123a17d1242ab1445b195cfc74b8817ce
TwoShot22 <mansoo0621@naver.com>
2023-12-17 22:58:07 +0900

    refactor: queryclient 객체를 App 컴포넌트 외부로 옮긴다

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,16 @@ import Reset from '@/styles/reset'
 import Normalize from '@/styles/normalize'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-function App() {
-	const queryClient = new QueryClient({
-		defaultOptions: {
-			queries: {
-				refetchOnWindowFocus: false,
-				retry: 2,
-			},
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+			retry: 2,
 		},
-	})
+	},
+})
 
+function App() {
 	return (
 		<QueryClientProvider client={queryClient}>
 			<Reset />
