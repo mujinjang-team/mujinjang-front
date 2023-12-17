13adc39b6566884b08b3e612a261bce1d81b81e3
TwoShot22 <mansoo0621@naver.com>
2023-12-17 19:56:07 +0900

    ui: admin 쿠폰 발급 페이지의 레이아웃과 디자인을 구현한다

--- src/components/admin/coupon/create/AdminCouponCreate.styles.ts
+++ src/components/admin/coupon/create/AdminCouponCreate.styles.ts
@@ -6,30 +6,56 @@ export const AdminCouponCreateContainer = styled.div`
 	width: 100%;
 	height: 100%;
 	flex-direction: column;
-	justify-content: center;
-	padding: 0 12px;
-	gap: 60px;
+	padding: 20px 12px 0 12px;
 `
 
-export const AdminCreateAmountInputContainer = styled.div`
+export const AdminCouponCreateTitle = styled.div`
+	font-size: 2rem;
+	font-weight: bold;
+	margin-bottom: 28px;
+`
+
+export const AdminCouponCreateContentWrapper = styled.div`
 	display: flex;
 	flex-direction: column;
-	justify-content: center;
-	align-items: center;
-	gap: 28px;
 `
 
-export const AdminCouponCreateAmountGuideText = styled.span`
-	font-size: 32px;
+export const AdminCouponCreateContentTitle = styled.div`
+	font-size: 1.2rem;
 	font-weight: 500;
+	margin-bottom: 20px;
 `
 
-export const AdminCouponCreateAmountInput = styled(TextField).attrs({
-	type: 'number',
-	placeholder: 'Type Number',
-})`
+export const AdminCouponCreateContentContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 12px;
+	margin-bottom: 48px;
+`
+
+export const AdminCouponCreateContentItemWrapper = styled.div`
+	display: flex;
+	gap: 16px;
+	align-items: center;
+`
+
+export const AdminCouponCreateContentItemTitle = styled.div`
+	display: inline-flex;
+	flex-basis: 150px;
+	font-size: 1rem;
+`
+
+export const AdminCouponCreateContentItem = styled.div`
+	display: inline-flex;
+	justify-content: start;
+	width: 100%;
+`
+
+export const StyledTextInput = styled(TextField)`
+	width: 100%;
+
 	* {
-		font-size: 20px !important;
+		font-size: 1rem;
 	}
 `
 
--- src/pages/admin/AdminCouponCreate.tsx
+++ src/pages/admin/AdminCouponCreate.tsx
@@ -1,31 +1,65 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import {
 	AdminCouponCreateButtonContainer,
 	AdminCouponCreateContainer,
-	AdminCouponCreateAmountGuideText,
-	AdminCouponCreateAmountInput,
-	AdminCreateAmountInputContainer,
+	AdminCouponCreateTitle,
+	AdminCouponCreateContentWrapper,
+	AdminCouponCreateContentTitle,
+	AdminCouponCreateContentContainer,
+	AdminCouponCreateContentItemWrapper,
+	AdminCouponCreateContentItemTitle,
+	AdminCouponCreateContentItem,
+	StyledTextInput,
 } from '@/components/admin/coupon/create/AdminCouponCreate.styles'
 import Button from '@/components/common/Button'
 
 const AdminCouponCreate = () => {
-	const inputRef = useRef<HTMLInputElement>()
-
 	return (
 		<AdminCouponCreateContainer>
-			<AdminCreateAmountInputContainer>
-				<AdminCouponCreateAmountGuideText>발급할 쿠폰의 수량 입력</AdminCouponCreateAmountGuideText>
-				<AdminCouponCreateAmountInput inputRef={inputRef} defaultValue={0} />
-			</AdminCreateAmountInputContainer>
+			<AdminCouponCreateTitle>쿠폰 발급</AdminCouponCreateTitle>
+			<AdminCouponCreateContentWrapper>
+				<AdminCouponCreateContentTitle>쿠폰 발급에 필요한 정보 입력</AdminCouponCreateContentTitle>
+			</AdminCouponCreateContentWrapper>
+			<AdminCouponCreateContentContainer>
+				<AdminCouponCreateContentItemWrapper>
+					<AdminCouponCreateContentItemTitle>쿠폰 코드</AdminCouponCreateContentItemTitle>
+					<AdminCouponCreateContentItem>
+						<StyledTextInput type="text" required />
+					</AdminCouponCreateContentItem>
+				</AdminCouponCreateContentItemWrapper>
+				<AdminCouponCreateContentItemWrapper>
+					<AdminCouponCreateContentItemTitle>쿠폰 이름</AdminCouponCreateContentItemTitle>
+					<AdminCouponCreateContentItem>
+						<StyledTextInput type="text" required />
+					</AdminCouponCreateContentItem>
+				</AdminCouponCreateContentItemWrapper>
+				<AdminCouponCreateContentItemWrapper>
+					<AdminCouponCreateContentItemTitle>쿠폰 타입</AdminCouponCreateContentItemTitle>
+					<AdminCouponCreateContentItem>
+						<StyledTextInput type="text" required />
+					</AdminCouponCreateContentItem>
+				</AdminCouponCreateContentItemWrapper>
+				<AdminCouponCreateContentItemWrapper>
+					<AdminCouponCreateContentItemTitle>쿠폰 할인 수치</AdminCouponCreateContentItemTitle>
+					<AdminCouponCreateContentItem>
+						<StyledTextInput type="number" required />
+					</AdminCouponCreateContentItem>
+				</AdminCouponCreateContentItemWrapper>
+				<AdminCouponCreateContentItemWrapper>
+					<AdminCouponCreateContentItemTitle>쿠폰 발급 수량</AdminCouponCreateContentItemTitle>
+					<AdminCouponCreateContentItem>
+						<StyledTextInput type="number" required />
+					</AdminCouponCreateContentItem>
+				</AdminCouponCreateContentItemWrapper>
+			</AdminCouponCreateContentContainer>
 			<AdminCouponCreateButtonContainer>
 				<Button
 					variant="black"
 					onClick={() => {
 						console.log('Clicked')
-						console.log(inputRef.current?.value)
 					}}
 				>
-					쿠폰 사용하기
+					쿠폰 발급하기
 				</Button>
 			</AdminCouponCreateButtonContainer>
 		</AdminCouponCreateContainer>
